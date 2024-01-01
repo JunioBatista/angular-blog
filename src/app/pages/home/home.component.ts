6c73d8a7eb2e09df55e948f7a67affadda941444
JunioBatista <juniorjob21@gmail.com>
2024-01-01 17:19:39 -0300

    component dynamics properties

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+
+interface newsDataObject {
+  coverPhoto: string
+  title: string
+}
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+  newsList: newsDataObject[] =  [
+    {
+      coverPhoto: 'https://picsum.photos/id/575/500/300',
+      title: 'Angular lança novas features.'
+    },
+    {
+      coverPhoto: 'https://picsum.photos/id/124/500/300',
+      title: 'Conheça o PC mais caro do mundo.'
+    },  
+    {
+      coverPhoto: 'https://picsum.photos/id/254/500/300',
+      title: "Nova invenção promete superar IA's "
+    }
+  ]
 
   constructor() { }
 
